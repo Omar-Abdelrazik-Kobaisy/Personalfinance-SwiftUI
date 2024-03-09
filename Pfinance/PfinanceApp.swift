03f742a413c5cefa2a3bfaf62799975854262ab8
omarAbdelrazik <omar.abdelrazik98@gmail.com>
2024-03-09 10:42:07 +0200

    Initial Commit

---
+++ Pfinance/PfinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PfinanceApp.swift
+//  Pfinance
+//
+//  Created by Omar on 09/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PfinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
